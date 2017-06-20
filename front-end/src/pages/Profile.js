68304f8509d0794e8f6f1e57dbdfec8d099501a2
Tomi Kaistila <tomi.kaistila@gmail.com>
2017-06-20 18:19:35 +0300

    Profile view done

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -3,9 +3,22 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
+import _ from 'lodash';
 import APIFetch from '../lib/Fetch';
 
 import '../css/User.css';
+
+class Stars extends React.Component {
+    render() {
+		return (
+			<div>
+				{_.times(this.props.total, i =>
+					<img src="/i/star.png" alt="" />
+				)}
+			</div>
+		)
+    }
+}
 
 class User extends React.Component {
 	constructor() {
@@ -26,17 +39,14 @@ class User extends React.Component {
 		return (
 			<Grid className="block">
 				<Row className="show-grid">
-					<Col mdOffset={3} md={8}><div id="username">Heikki Keskari</div></Col>
+					<Col mdOffset={3} md={8}><div id="username" className="top">{this.props.seller}</div></Col>
 				</Row>
 				<Row className="show-grid">
 					<Col mdOffset={3} md={8}>
 						<div>
-							<img src="/i/star.png" alt="" />
-							<img src="/i/star.png" alt="" />
-							<img src="/i/star.png" alt="" />
-							<img src="/i/star.png" alt="" />
+							<Stars total={this.props.average} />
 							<a id="reviews_total">
-								(132 reviews)
+								({this.props.reviews} reviews)
 								<span><img src="/i/breakdown.png" alt="" /></span>
 							</a>
 						</div>
@@ -52,45 +62,37 @@ class Review extends React.Component {
 		return (
 			<Grid className="block">
 				<Row>
-					<Col mdOffset={3} md={8}><div className="text bold">Helkama Yoker 16" polkupyörä (09.05.2017 08:25)</div></Col>
+					<Col mdOffset={3} md={8}><div className="text bold">{this.props.data.item} ({this.props.data.saved})</div></Col>
 				</Row>
 				<Row>
-					<Col mdOffset={3} md={1}>
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
+					<Col mdOffset={3} md={2}>
+						<Stars total={this.props.data.overall} />
 					</Col>
 					<Col md={6}><span className="text">Overall</span></Col>
 				</Row>
 				<Row>
-					<Col mdOffset={3} md={1}>
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
+					<Col mdOffset={3} md={2}>
+						<Stars total={this.props.data.communication} />
 					</Col>
 					<Col md={6}><div className="text">Communication</div></Col>
 				</Row>
 				<Row>
-					<Col mdOffset={3} md={1}>
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
+					<Col mdOffset={3} md={2}>
+						<Stars total={this.props.data.shipping} />
 					</Col>
 					<Col md={6}><div className="text">Shipping</div></Col>
 				</Row>
 				<Row className="sblock">
-					<Col mdOffset={3} md={1}>
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
-						<img src="/i/star.png" alt="" />
+					<Col mdOffset={3} md={2}>
+						<Stars total={this.props.data.condition} />
 					</Col>
 					<Col md={6}><div className="text">Condition</div></Col>
 				</Row>
 				<Row className="sblock">
-					<Col mdOffset={3} md={6}><div className="text">Buyer: Nirri Mäkelä</div></Col>
+					<Col mdOffset={3} md={6}><div className="text">Buyer: {this.props.data.sender}</div></Col>
 				</Row>
 				<Row>
-					<Col mdOffset={3} md={6}><div className="text">Comment: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div></Col>
+					<Col mdOffset={3} md={6}><div className="text">Comment: {this.props.data.comment}</div></Col>
 				</Row>
 			</Grid>
 		);
@@ -100,7 +102,7 @@ class Review extends React.Component {
 class Profile extends React.Component {
 	constructor() {
 		super();
-		this.state = {store: false}
+		this.state = {store: false, size: 0}
 	}
 
 	store(data) {
@@ -110,18 +112,22 @@ class Profile extends React.Component {
 	componentDidMount() {
 		APIFetch().get("/ratings/getbyseller/Heikki Keskari").then(response => {
             this.store(response.data)
+			this.setState({size: Object.keys(response.data).length})
         })
 	}
 
 	componentDidUpdate() {
-		console.log(this.state.store)
+		//console.log(this.state.store)
 	}
 
     render() {
+		//console.log(this.state.size)
 		return (
 			<div>
-				<User />
-				<Review />
+				<User seller="Heikki Henkari" average="4" reviews={this.state.size} />
+				{Object.keys(this.state.store).map((name) =>
+					<Review data={this.state.store[name]} />
+				)}
 			</div>
 		);
     }
--- mock-data/ratings.sql
+++ mock-data/ratings.sql
@@ -12,4 +12,4 @@ create table ratings (
 );
 insert into ratings (item, sender, seller, saved, overall, communication, shipping, condition, comment) values ('Helkama Yoker 16" polkupyörä', 'Nirri Mäkelä', 'Heikki Keskari', '14.05.2017 08:25', 3, 2, 3, 3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
 insert into ratings (item, sender, seller, saved, overall, communication, shipping, condition, comment) values ('Yosemite 24 Mini Jopo', 'Mörri Mäkelä', 'Heikki Keskari', '03.06.2017 14:41', 4, 3, 3, 4, 'Duis leo nisi, vulputate vitae lectus in, commodo mollis tortor.');
-insert into ratings (item, sender, seller, saved, overall, communication, shipping, condition, comment) values ('E-Scooter-SegWheel 800W', 'Helena Henkseli', 'Heikki Keskari', '28.05.2017 19:04', 5, 5, 5, 5, 'Fusce at odio eu tellus pretium dignissim ut vel risus.');
+insert into ratings (item, sender, seller, saved, overall, communication, shipping, condition, comment) values ('E-Scooter-SegWheel 800W', 'Helena Henkseli', 'Heikki Keskari', '28.05.2017 19:04', 4, 2, 3, 5, 'Fusce at odio eu tellus pretium dignissim ut vel risus.');
